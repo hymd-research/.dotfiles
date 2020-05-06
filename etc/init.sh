080c819e68291cacf090fcc83f4e970db91e06f1
hymd <hymd@hymdResearch.local>
2020-05-06 16:57:58 +0900

    MOD: requirements

--- etc/init.sh
+++ etc/init.sh
@@ -3,6 +3,7 @@
 "$(dirname $0)"/requirements.sh
 
 setopt EXTENDED_GLOB
-for rcfile in "${HOME}"/.dotfiles/.git(.N); do
+for rcfile in "${HOME}"/.dotfiles/^.git(.N); do
+  echo "$rcfile"
   ln -s "$rcfile" "${HOME}/${rcfile:t}"
 done
--- etc/requirements.sh
+++ etc/requirements.sh
@@ -1,6 +1,6 @@
 #!/usr/local/bin/zsh
 
-if [ ! -d "${HOME}"/.cache/.dein ]; then
+if [ ! -d "${HOME}"/.cache/dein ]; then
     curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ./installer.sh
     sh ./installer.sh "${HOME}/.cache/dein"
     rm ./installer.sh
