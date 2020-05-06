f08b1a9383deee9bf278b2089640ea8ca78998ea
hymd <hymd@hymdResearch.local>
2020-05-06 16:50:52 +0900

    MOD: requirements

--- etc/requirements.sh
+++ etc/requirements.sh
@@ -1,12 +1,12 @@
 #!/usr/local/bin/zsh
 
-if [! -e "${HOME}"/.cache/.dein]; then
+if [ ! -d "${HOME}"/.cache/.dein]; then
     curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ./installer.sh
     sh ./installer.sh "${HOME}/.cache/dein"
     rm ./installer.sh
 fi
 
-if [! -e "${HOME}"/.zprezto]; then
+if [ ! -d "${HOME}"/.zprezto]; then
     git clone --recursive https://github.com/sorin-ionescu/prezto.git "${HOME}/.zprezto"
 fi
 
