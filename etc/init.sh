c0f5956733e295b896f555725ccaf95ae46668fa
hymd <hymd@hymdResearch.local>
2020-05-06 17:02:56 +0900

    MOD: requirements

--- etc/init.sh
+++ etc/init.sh
@@ -3,7 +3,7 @@
 "$(dirname $0)"/requirements.sh
 
 setopt EXTENDED_GLOB
-for rcfile in "${HOME}"/.dotfiles/^.git(.N); do
-  echo "$rcfile"
+for rcfile in "${HOME}"/.dotfiles/.??*; do
+  if  [[ "$rcfile" == ".git" ]] && continue 
   ln -s "$rcfile" "${HOME}/${rcfile:t}"
 done
